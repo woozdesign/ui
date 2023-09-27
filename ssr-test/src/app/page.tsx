f24ae43afbadbbb0836a8af097f178c7f378173c
Kwanwoo Kim <kim.kw8012@gmail.com>
2023-09-27 14:08:42 -0700

    .

--- ssr-test/src/app/page.tsx
+++ ssr-test/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             By <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
           </a>
 
-          <Button href="/" color={'gray'}>
+          <Button href="/" color={'gray'} variant={'outlined'} highContrast>
             asdf
           </Button>
         </div>
