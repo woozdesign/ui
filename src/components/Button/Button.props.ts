63f27e9c7ca7808b9797141e2ebee454905b1609
Kwanwoo Kim <kim.kw8012@gmail.com>
2023-09-28 12:53:48 -0700

    changed props

--- src/components/Button/Button.props.ts
+++ src/components/Button/Button.props.ts
@@ -2,7 +2,7 @@ import { ColorProp, SizeProp } from '../../utils';
 
 export type ButtonClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => void;
 export type AnchorClickHandler = (event: React.MouseEvent<HTMLAnchorElement>) => void;
-export interface ButtonProps extends ColorProp, SizeProp {
+export interface BaseProps extends ColorProp, SizeProp {
   variant?: 'primary' | 'outlined' | 'secondary' | 'text' | 'icon';
   buttonType?: 'button' | 'reset' | 'submit';
   block?: boolean;
@@ -13,6 +13,15 @@ export interface ButtonProps extends ColorProp, SizeProp {
   iconAppend?: JSX.Element;
   children?: React.ReactNode;
   href?: string;
-  className?: string;
-  onClick?: ButtonClickHandler | AnchorClickHandler;
 }
+
+export interface ButtonSpecificProps extends BaseProps, Omit<React.HTMLProps<HTMLButtonElement & HTMLAnchorElement>, 'color' | 'shape' | 'size' | 'type'> {
+  onClick?: ButtonClickHandler;
+}
+
+export interface AnchorSpecificProps extends BaseProps, Omit<React.HTMLProps<HTMLButtonElement & HTMLAnchorElement>, 'color' | 'shape' | 'size' | 'type'> {
+  href?: string;
+  onClick?: AnchorClickHandler;
+}
+
+export type ButtonProps = ButtonSpecificProps | AnchorSpecificProps;
