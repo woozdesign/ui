15fbeff8d16305146d5ce6691660fb67c4886c5c
Kwanwoo Kim <kim.kw8012@gmail.com>
2023-09-28 10:54:54 -0700

    build changed

--- dist/components/Button/Button.props.d.ts
+++ dist/components/Button/Button.props.d.ts
@@ -17,6 +17,6 @@ export interface ButtonSpecificProps extends BaseProps, Omit<React.HTMLProps<HTM
     onClick?: ButtonClickHandler;
 }
 export interface AnchorSpecificProps extends BaseProps, Omit<React.HTMLProps<HTMLButtonElement & HTMLAnchorElement>, 'color' | 'shape' | 'size' | 'type'> {
-    href: string;
+    href?: string;
     onClick?: AnchorClickHandler;
 }
