efe1f2b88dd76a2f6c7d3f45bc37a5198f368218
Kwanwoo Kim <kim.kw8012@gmail.com>
2023-10-08 15:24:41 -0700

    Tab added

--- src/components/index.ts
+++ src/components/index.ts
@@ -22,5 +22,6 @@ export { default as TextField } from './TextField';
 export { default as Theme } from './Theme';
 export { default as Tooltip } from './Tooltip';
 export { default as Typography } from './Typography';
+export { default as Tab } from './Tab';
 
 console.log('%cDesigned with WoozDesign', 'font-size: 12px;font-family: monospace;background: black;display: inline-block;color: white;padding: 15px;border: 2px solid white;');
