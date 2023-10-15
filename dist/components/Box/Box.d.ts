dc627a9799a168986278f724ff2fb39b96f1a926
Kwanwoo Kim <kim.kw8012@gmail.com>
2023-10-15 16:19:32 -0700

    box props changed

--- dist/components/Box/Box.d.ts
+++ dist/components/Box/Box.d.ts
@@ -1,15 +1,4 @@
-import React, { FC } from 'react';
-interface BoxProps {
-    margin?: React.CSSProperties['margin'];
-    padding?: React.CSSProperties['padding'];
-    width?: React.CSSProperties['width'];
-    height?: React.CSSProperties['height'];
-    backgroundColor?: React.CSSProperties['backgroundColor'];
-    color?: React.CSSProperties['color'];
-    borderRadius?: React.CSSProperties['borderRadius'];
-    style?: React.CSSProperties;
-    className?: string;
-    children?: React.ReactNode;
-}
+import { FC } from 'react';
+import { BoxProps } from './Box.props';
 declare const Box: FC<BoxProps>;
 export default Box;
---
+++ src/components/Box/Box.props.ts
@@ -0,0 +1,12 @@
+export interface BoxProps {
+  margin?: React.CSSProperties['margin'];
+  padding?: React.CSSProperties['padding'];
+  width?: React.CSSProperties['width'];
+  height?: React.CSSProperties['height'];
+  backgroundColor?: React.CSSProperties['backgroundColor'];
+  color?: React.CSSProperties['color'];
+  borderRadius?: React.CSSProperties['borderRadius'];
+  style?: React.CSSProperties;
+  className?: string;
+  children?: React.ReactNode;
+}
