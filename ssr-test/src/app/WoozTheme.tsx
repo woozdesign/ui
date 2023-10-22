6bc4135695f7f88aa7c64527168ec6ac1331c373
Kwanwoo Kim <kim.kw8012@gmail.com>
2023-10-21 18:37:20 -0700

    fixed imports

--- ssr-test/src/app/WoozTheme.tsx
+++ ssr-test/src/app/WoozTheme.tsx
@@ -5,5 +5,5 @@ import { ThemeProvider } from '@woozdesign/ui';
 import '@woozdesign/ui/styles.css';
 
 export const RootStyleRegistry = ({ children }: React.PropsWithChildren) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return <ThemeProvider appearance="light">{children}</ThemeProvider>;
 };
--- ssr-test/src/app/page.tsx
+++ ssr-test/src/app/page.tsx
@@ -1,10 +1,19 @@
 'use client';
 
 import { Icon } from '@woozdesign/icons';
-import { AppBar, Badge, Button, Container, Drawer, IconButton, Row, Tab, Typography, updateThemeAppearanceClass, useThemeContext, useToast } from '@woozdesign/ui';
+import { AppBar, Badge, Button, Card, Container, Drawer, IconButton, Row, Tab, Typography, updateThemeAppearanceClass, useThemeContext, useToast } from '@woozdesign/ui';
 import { useState } from 'react';
 
 export default function Home() {
+  const { appearance, onAppearanceChange } = useThemeContext();
+  const onAppearanceToggle = () => {
+    console.log('appearance: ', appearance);
+    if (appearance === 'dark') {
+      onAppearanceChange('light');
+    } else {
+      onAppearanceChange('dark');
+    }
+  };
   return (
     <>
       <main style={{ marginTop: '64px' }}>
@@ -12,7 +21,10 @@ export default function Home() {
           <Row>
             <Badge mt={{ xs: '0', md: '6', lg: '12' }} label={'Badge Test'}></Badge>
           </Row>
-          <Row>Test</Row>
+          <Row>
+            <Typography.Header>Playground</Typography.Header>
+            <Button onClick={onAppearanceToggle}>Toggle</Button>
+          </Row>
         </Container>
       </main>
     </>
