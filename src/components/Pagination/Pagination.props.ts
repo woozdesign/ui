692e3ae00a5e25d2aef72bdbebb6331c2495cf7a
Kwanwoo Kim <kim.kw8012@gmail.com>
2023-10-31 15:27:19 -0700

    pagination shadow props

--- src/components/Pagination/Pagination.props.ts
+++ src/components/Pagination/Pagination.props.ts
@@ -1,6 +1,6 @@
-import { ColorProp, HighContrastProp, MarginProp, RadiusProp, SizeProp } from '@/utils';
+import { ColorProp, HighContrastProp, MarginProp, RadiusProp, ShadowProp, SizeProp } from '@/utils';
 
-export interface PaginationProps extends ColorProp, HighContrastProp, RadiusProp, SizeProp, MarginProp {
+export interface PaginationProps extends ColorProp, HighContrastProp, RadiusProp, SizeProp, MarginProp, ShadowProp {
   totalRecords: number;
   recordsPerPage: number;
   onPageChanged: (pageNumber: number) => void;
